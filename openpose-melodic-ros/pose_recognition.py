bf75f1756d91cf012602744c71d0f90818fc735f
Nick Huppert <s3729119@student.rmit.edu.au>
2020-08-13 14:41:46 +1000

    posenet to python

---
+++ openpose-melodic-ros/pose_recognition.py
@@ -0,0 +1,8 @@
+import sys
+import cv2
+import os
+from sys import platform
+import argparse
+import time
+sys.path.append('/openpose/openpose/python/openpose/Release')
+from openpose import pyopenpose as op
--- posenet/poser.py
+++ posenet/poser.py
@@ -1,1 +1,24 @@
-import cv2
+from flask import Flask, render_template, json, request
+from flask_cors import CORS, cross_origin
+
+app = Flask("__main__")
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/", methods=['GET', 'POST', 'OPTIONS'])
+@cross_origin()
+def index():
+    data = request.get_json()
+    for point in data:
+        print(point["keypoints"])
+    return "", 201
+
+
+
+@app.route("/pose", methods=['GET', 'POST', 'OPTIONS'])
+def coco():
+    print(request.get_json(force=True))
+
+
+app.run(debug=True)
