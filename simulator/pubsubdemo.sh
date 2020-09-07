761d5dae235e3d71ffce8b70d76ed1237f222784
Nick <s3729119@student.rmit.edu.au>
2020-09-07 15:20:32 +1000

    Small script change

--- simulator/pubsubdemo.sh
+++ simulator/pubsubdemo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /simulator/devel/setup.bash
-gzclient roslaunch rotors_gazebo crazyflie2_hovering_example.launch &
-gazebo roslaunch rotors_gazebo crazyflie2_hovering_example.launch &
+#gzclient &
+roslaunch rotors_gazebo crazyflie2_hovering_example.launch --wait &
 rosrun demo_listener demo_listener.py
