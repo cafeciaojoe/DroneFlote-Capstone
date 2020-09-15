b736a3aa4352a1e80444914c9e18c5d300282297
Nick Huppert <s3729119@student.rmit.edu.au>
2020-09-15 15:10:06 +1000

    Separating the pose logic from the simulator

---
+++ pose_parser/pubsubdemo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /parser/devel/setup.bash
+rosrun demo_listener demo_listener.py
--- simulator/startsim.sh
+++ simulator/startsim.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /simulator/devel/setup.bash
-roslaunch rotors_gazebo crazyflie2_hovering_example.launch --wait &
+roslaunch rotors_gazebo crazyflie2_hovering_example.launch --wait
 #roslaunch rotors_gazebo crazyflie2_hovering_example.launch enable_state_estimator:=true --wait &
-rosrun demo_listener demo_listener.py
