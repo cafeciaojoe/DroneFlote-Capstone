fc6135792afbd792acfd7bf8815bdee6f6307470
Nick Huppert <s3729119@student.rmit.edu.au>
2020-09-18 11:44:41 +1000

    Cleanup redundant files

--- openpose-melodic-ros/pose_recognition.py
+++
@@ -1,8 +0,0 @@
-import sys
-import cv2
-import os
-from sys import platform
-import argparse
-import time
-sys.path.append('/openpose/openpose/python/openpose/Release')
-from openpose import pyopenpose as op
